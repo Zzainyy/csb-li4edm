982037bf99e08c47f91af8d81ea03ea3006f3c21
Code Sandbox <hello@codesandbox.io>
2022-07-08 16:13:25 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.706112c5ee03db2d4c8555e0c2d535d3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a4d1e5ba3f4c22c13ccd23d9fe429ad",
+    "url": "https://zzainyy.github.io/csb-li4edm/index.html"
+  },
+  {
+    "revision": "b466afdca85cbc38cf50",
+    "url": "https://zzainyy.github.io/csb-li4edm/static/js/2.058c2d5f.chunk.js"
+  },
+  {
+    "revision": "8d31d9e9e66690e9c2ab",
+    "url": "https://zzainyy.github.io/csb-li4edm/static/js/main.cb67dc24.chunk.js"
+  },
+  {
+    "revision": "df1aea7491f40b171007",
+    "url": "https://zzainyy.github.io/csb-li4edm/static/js/runtime-main.1f50aab5.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://zzainyy.github.io/csb-li4edm/precache-manifest.706112c5ee03db2d4c8555e0c2d535d3.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://zzainyy.github.io/csb-li4edm/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
